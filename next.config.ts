b88215125e8e6305ed4841addd0029324b77d49d
Yusuf <abdullahyusufb@gmail.com>
2026-03-22 10:12:13 +0700

    Extract web.zip and add source code

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,22 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
+
+  // Tambahan redirect untuk mengatasi error 404 dari URL /home di Google Search Console
+  async redirects() {
+    return [
+      {
+        source: '/home',
+        destination: '/',
+        permanent: true, // Redirect 301 (SEO Friendly)
+      },
+      {
+        source: '/home/', // Menangkap URL jika ada garis miring di akhir
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
