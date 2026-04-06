3a70a399516d931c93b67692198e708af5ce06c3
Yusuf Budi Abdullah <budiabdullahy@gmail.com>
2026-04-06 14:07:59 +0700

    Update page.tsx

--- src/app/produk/[slug]/page.tsx
+++ src/app/produk/[slug]/page.tsx
@@ -40,8 +40,11 @@ export default function ProductDetailPage() {
     { nama: 'Bima', no: '6289637144539' },
     { nama: 'Kafi', no: '6281329095557' },
     { nama: 'Nabila', no: '6283103278381' },
-    { nama: 'Melly', no: '62895417267981' }
+    { nama: 'Melly', no: '62895417267981' },
+    { nama: 'Alma', no: '6282134148101' },
+    { nama: 'Indah', no: '6282135245314' }
   ];
+
 
   const getRandomWANumber = () => {
     const randomIndex = Math.floor(Math.random() * waTeam.length);
